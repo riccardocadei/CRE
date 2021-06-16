e3c2154026f0c1b47a2e9dbfef964d5d2cbb7d13
Daniela Garcia <danielagarcia@surgofoundation.org>
2021-06-16 15:25:35 -0400

    Comment out sensitivity analysis until function is done

--- R/cre.R
+++ R/cre.R
@@ -22,7 +22,7 @@
 #'
 cre <- function(y, z, X, ratio_dis, ite_method_dis, ite_method_inf, ntrees, min_nodes, max_nodes, rules_method, t) {
   # Step 1: Split data
-  print("Step 1: Splitting Data")
+  message("Step 1: Splitting Data")
   if (!("matrix" %in% class(X))) {
     X <- as.matrix(X)
   }
@@ -40,42 +40,42 @@ cre <- function(y, z, X, ratio_dis, ite_method_dis, ite_method_inf, ntrees, min_
   X_inf <- inference[,3:ncol(discovery)]
 
   ###### Discovery ######
-  print("Conducting Discovery Subsample Analysis")
+  message("Conducting Discovery Subsample Analysis")
 
   # Step 2: Estimate ITE
-  print("Step 2: Estimating ITE")
+  message("Step 2: Estimating ITE")
   ite_list_dis <- CRE::estimate_ite(y_dis, z_dis, X_dis, ite_method_dis)
   ite_dis <- ite_list_dis[["ite"]]
   ite_std_dis <- ite_list_dis[["ite_std"]]
 
   # Step 3: Generate rules list
-  print("Step 3: Generating Initial Causal Rules")
+  message("Step 3: Generating Initial Causal Rules")
   initial_rules_dis <- CRE::generate_rules(X_dis, ite_std_dis, ntrees, min_nodes, max_nodes)
 
   # Step 4: Generate rules matrix
-  print("Step 4: Generating Causal Rules Matrix")
+  message("Step 4: Generating Causal Rules Matrix")
   rules_all_dis <- CRE::generate_rules_matrix(X_dis, initial_rules_dis, t)
   rules_matrix_dis <- rules_all_dis[["rules_matrix"]]
   rules_matrix_std_dis <- rules_all_dis[["rules_matrix_std"]]
   rules_list_dis <- rules_all_dis[["rules_list"]]
 
   # Step 5: Select important rules
-  print("Step 5: Selecting Important Causal Rules")
+  message("Step 5: Selecting Important Causal Rules")
   select_rules_dis <- CRE::select_causal_rules(rules_matrix_std_dis, rules_list_dis, ite_std_dis, rules_method)
   select_rules_matrix_dis <- rules_matrix_dis[,which(rules_list_dis %in% select_rules_dis)]
   select_rules_matrix_std_dis <- rules_matrix_std_dis[,which(rules_list_dis %in% select_rules_dis)]
 
   ###### Inference ######
-  print("Conducting Inference Subsample Analysis")
+  message("Conducting Inference Subsample Analysis")
 
   # Step 2: Estimate ITE
-  print("Step 2: Estimating ITE")
+  message("Step 2: Estimating ITE")
   ite_list_inf <- CRE::estimate_ite(y_inf, z_inf, X_inf, ite_method_inf)
   ite_inf <- ite_list_inf[["ite"]]
   ite_std_inf <- ite_list_inf[["ite_std"]]
 
   # Step 6: Estimate CATE
-  print("Step 6: Estimating CATE")
+  message("Step 6: Estimating CATE")
   rules_all_inf <- CRE::generate_rules_matrix(X_inf, select_rules_dis, t)
   rules_list_inf <- rules_all_inf[["rules_list"]]
   rules_matrix_inf <- rules_all_inf[["rules_matrix"]]
@@ -83,10 +83,11 @@ cre <- function(y, z, X, ratio_dis, ite_method_dis, ite_method_inf, ntrees, min_
   cate_inf <- CRE::estimate_cate(ite_std_inf, rules_matrix_std_inf)
 
   # Step 7: Conduct sensitivity analysis
-  # TBD
+  message("Step 7: Conducting Sensitivity Analysis")
+  # sensitivity_results <- analyze_sensitivity(ite_std_inf, rules_matrix_std_inf)
 
   # Return Results
-  print("CRE method complete. Returning results.")
+  message("CRE method complete. Returning results.")
   cre_results <- list(select_rules = rules_list_inf, cate = cate_inf)
   return(cre_results)
 }
