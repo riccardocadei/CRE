6574a945e96021172c14c565680dcae393b1ff1f
Daniela Garcia <danielagarcia@surgofoundation.org>
2021-06-16 15:26:05 -0400

    Attempt to construct function in the interim

--- R/analyze_sensitivity.R
+++ R/analyze_sensitivity.R
@@ -5,15 +5,43 @@
 #' Method for analyzing the sensitivity of the estimates of the causal rule-specific treatment effects
 #'
 #' @param ite_std the standardized ITE
-#' @param X the features matrix
-#' @param select_rules a list of select causal rules
+#' @param rules_matrix_std the standardized causal rules matrix
 #'
-#' @return a matrix containing the results of the sensitivity analysis
+#' @return a list containing the results of the sensitivity analysis
 #'
 #' @export
 #'
 #' @examples
 #' TBD
-analyze_sensitivity <- function(ite_std, X, select_rules) {
-  # TBD
+#'
+analyze_sensitivity <- function(ite_std, rules_matrix_std) {
+  # Initialize parameters
+  all_zero <- FALSE
+  sensitivity_results <- list()
+  i <- 0
+
+  # Continue testing different values of lambda until all confidence intervals contain 0
+  while (all_zero == FALSE) {
+    lambda <- 1.0 + (0.01 * i)
+
+    # Generate confidence intervals using current lambda
+    # model_temp <- TBD
+    ci_temp <- as.data.frame(confint(model_temp)) %>% filter(complete.cases(.))
+
+    # Add confidence intervals to list
+    sensitivity_results[[as.character(lambda)]] <- ci_temp
+
+    # Check if 0 is present in all confidence intervals for that iteration
+    zero_in_range <- vector(length = nrow(ci_temp))
+    for (l in 1:nrow(ci_temp)) {
+      zero_in_range[l] <- between(0, ci_temp[l,1], ci_temp[l,2])
+    }
+    if (all(zero_in_range)) {
+      all_zero <- TRUE
+    }
+
+    # Increase i
+    i <- i + 1
+  }
+  return(sensitivity_results)
 }
