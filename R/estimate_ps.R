3b37b3752cd7f1dec5ecec111bb2bf665859be69
Daniela Garcia <danielagarcia@surgofoundation.org>
2021-07-06 15:53:36 -0400

    Add helper function to estimate the propensity score (ps)

---
+++ R/estimate_ps.R
@@ -0,0 +1,22 @@
+#' @title
+#' Estimate the Propensity Score
+#'
+#' @description
+#' Method for estimating the Propensity Score given a treatment vector and features matrix
+#'
+#' @param z the treatment vector
+#' @param X the features matrix
+#'
+#' @return a list of propensity score estimates
+#'
+#' @export
+#'
+#' @examples
+#' TBD
+#'
+estimate_ps <- function(z, X) {
+  propscore_model <- glm(z ~ X, family = binomial)
+  logit_ps <- predict(propscore_model)
+  est_ps <- exp(logit_ps) / (1 + exp(logit_ps))
+  return(est_ps)
+}
