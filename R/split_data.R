d9dd40325ba06cbe347c1acf6f8bf76526b85f22
Daniela Garcia <danielagarcia@surgofoundation.org>
2021-07-07 12:02:30 -0400

    Fix typo

--- R/split_data.R
+++ R/split_data.R
@@ -7,7 +7,7 @@
 #' @param y the observed response vector
 #' @param z the treatment vector
 #' @param X the features matrix
-#' @param raio_dis the ratio of data delegated to the discovery subsample
+#' @param ratio_dis the ratio of data delegated to the discovery subsample
 #'
 #' @return a list containing the discovery and inference subsamples
 #'
