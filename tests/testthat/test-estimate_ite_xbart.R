20c7157ef1c7f78c59edcb4aaddc5098bb076bda
Daniela Garcia <danielagarcia@surgofoundation.org>
2021-07-09 17:21:40 -0400

    Write initial test

---
+++ tests/testthat/test-estimate_ite_xbart.R
@@ -0,0 +1,20 @@
+test_that("BART ITE Estimated Correctly", {
+  # Generate sample data
+  dataset_cont <- generate_cre_dataset(n = 1000, rho = 0, n_rules = 2, effect_size = 0.5, binary = FALSE, seed = 2021)
+  y <- dataset_cont[["y"]]
+  z <- dataset_cont[["z"]]
+  X <- dataset_cont[["X"]]
+  include_ps <- TRUE
+
+  # Incorrect data inputs
+  suppressWarnings(expect_error(estimate_ite_xbart(y = "test", z, X, include_ps)))
+  suppressWarnings(expect_error(estimate_ite_xbart(y, z = "test", X, include_ps)))
+  suppressWarnings(expect_error(estimate_ite_xbart(y, z, X = "test", include_ps)))
+  suppressWarnings(expect_error(estimate_ite_xbart(y, z, X, include_ps = "test")))
+  suppressWarnings(expect_error(estimate_ite_xbart(y, z, X, include_ps = NA)))
+
+  # Correct outputs
+  ite_result <- estimate_ite_xbart(y, z, X, include_ps)
+  expect_true(length(ite_result) == length(y))
+  expect_true(class(ite_result) == "numeric")
+})
