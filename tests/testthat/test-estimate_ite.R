9fa3f2ffe605c6837e8e9a62fb87c5e5fae0b4e2
Daniela Garcia <danielagarcia@surgofoundation.org>
2021-07-09 17:19:19 -0400

    Write initial test

--- tests/testthat/test-estimate_ite.R
+++ tests/testthat/test-estimate_ite.R
@@ -1,3 +1,33 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("ITE Estimated Correctly", {
+  # Generate sample data
+  dataset_cont <- generate_cre_dataset(n = 1000, rho = 0, n_rules = 2, effect_size = 0.5, binary = TRUE, seed = 2021)
+  y <- dataset_cont[["y"]]
+  z <- dataset_cont[["z"]]
+  X <- dataset_cont[["X"]]
+  ite_method <- "xbart"
+  include_ps <- TRUE
+  binary <- TRUE
+
+  # Incorrect data inputs
+  suppressWarnings(expect_error(estimate_ite(y = "test", z, X, ite_method, include_ps, binary)))
+  suppressWarnings(expect_error(estimate_ite(y, z = "test", X, ite_method, include_ps, binary)))
+  suppressWarnings(expect_error(estimate_ite(y, z, X = "test", ite_method, include_ps, binary)))
+
+  # Incorrect ite_method input
+  suppressWarnings(expect_error(estimate_ite(y, z, X, ite_method = "test", include_ps, binary)))
+
+  # Incorrect include_ps input
+  suppressWarnings(expect_error(estimate_ite(y, z, X, ite_method, include_ps = "test", binary)))
+
+  # Incorrect binary input
+  suppressWarnings(expect_error(estimate_ite(y, z, X, ite_method, include_ps, binary = "test")))
+
+  # Correct outputs
+  ite_result <- estimate_ite(y, z, X, ite_method, include_ps, binary)
+  expect_true(length(ite_result) == 2)
+  expect_true(class(ite_result[[1]]) == "numeric")
+  expect_true(class(ite_result[[2]]) == "numeric")
+  expect_true(length(ite_result[[1]]) == length(y))
+  expect_true(length(ite_result[[2]]) == length(y))
+  expect_true(length(unique(ite_result[[1]])) %in% c(1, 2, 3))
 })
