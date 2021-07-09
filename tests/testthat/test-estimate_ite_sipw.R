9f29d33732e5281ad7d9a87c412c7f2701e89109
Daniela Garcia <danielagarcia@surgofoundation.org>
2021-07-09 17:20:33 -0400

    Write initial test

--- tests/testthat/test-estimate_ite_sipw.R
+++ tests/testthat/test-estimate_ite_sipw.R
@@ -1,3 +1,17 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("SIPW ITE Estimated Correctly", {
+  # Generate sample data
+  dataset_cont <- generate_cre_dataset(n = 1000, rho = 0, n_rules = 2, effect_size = 0.5, binary = FALSE, seed = 2021)
+  y <- dataset_cont[["y"]]
+  z <- dataset_cont[["z"]]
+  X <- dataset_cont[["X"]]
+
+  # Incorrect data inputs
+  suppressWarnings(expect_error(estimate_ite_or(y = "test", z, X)))
+  suppressWarnings(expect_error(estimate_ite_or(y, z = NA, X)))
+  suppressWarnings(expect_error(estimate_ite_or(y, z, X = "test")))
+
+  # Correct outputs
+  ite_result <- estimate_ite_or(y, z, X)
+  expect_true(length(ite_result) == length(y))
+  expect_true(class(ite_result) == "numeric")
 })
