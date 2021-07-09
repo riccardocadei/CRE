6aba3bf9e0abe22ac7f87f4b3c96c293c83bd948
Daniela Garcia <danielagarcia@surgofoundation.org>
2021-07-09 17:13:09 -0400

    Add additional arguments and checks

--- R/cre.R
+++ R/cre.R
@@ -16,13 +16,16 @@
 #' @param min_nodes the minimum size of the trees' terminal nodes
 #' @param max_nodes the maximum size of the trees' terminal nodes
 #' @param t the common support used in generating the causal rules matrix
+#' @param q the selection threshold used in selecting the causal rules
+#' @param rules_method the method for selecting causal rules with binary outcomes
 #'
 #' @return a list containing a select list of causal rules, Conditional Average Treatment Effect estimates, and a sensitivity analysis
 #'
 #' @export
 #'
 cre <- function(y, z, X, ratio_dis, ite_method_dis, ite_method_inf,
-                include_ps_dis = NA, include_ps_inf = NA, ntrees, min_nodes, max_nodes, t) {
+                include_ps_dis = NA, include_ps_inf = NA, ntrees, min_nodes,
+                max_nodes, t, q, rules_method) {
   # Check for correct numerical inputs
   if (class(y) != "numeric") stop("Invalid 'y' input. Please input a numeric vector.")
   if (class(z) != "integer") stop("Invalid 'z' input. Please input a binary treatment vector.")
@@ -40,29 +43,57 @@ cre <- function(y, z, X, ratio_dis, ite_method_dis, ite_method_inf,
   if (class(ntrees) != "numeric") stop("Invalid 'ntrees' input. Please input a number.")
   if (class(min_nodes) != "numeric") stop("Invalid 'min_nodes' input. Please input a number.")
   if (class(max_nodes) != "numeric") stop("Invalid 'max_nodes' input. Please input a number.")
-  if (class(t) != "numeric") stop("Invalid 'ntrees' input. Please input a number.")
+  if (class(t) != "numeric") stop("Invalid 't' input. Please input a number.")
+  if (class(q) != "numeric") stop("Invalid 'q' input. Please input a number.")
 
   # Check for correct ITE inputs
-  if (!(tolower(ite_method_dis) %in% c("ipw", "sipw", "or", "bart", "xbart", "bcf", "xbcf", "cf"))) {
+  ite_method_dis <- tolower(ite_method_dis)
+  if (!(ite_method_dis %in% c("ipw", "sipw", "or", "bart", "xbart", "bcf", "xbcf", "cf"))) {
     stop("Invalid ITE method for Discovery Subsample. Please choose from the following:
          'ipw', 'sipw', or, 'bart', 'xbart', 'bcf', 'xbcf', or 'cf'")
   }
-  if (!(tolower(ite_method_inf) %in% c("ipw", "sipw", "or", "bart", "xbart", "bcf", "xbcf", "cf"))) {
+  ite_method_inf <- tolower(ite_method_inf)
+  if (!(ite_method_inf %in% c("ipw", "sipw", "or", "bart", "xbart", "bcf", "xbcf", "cf"))) {
     stop("Invalid ITE method for Inference Subsample. Please choose from the following:
          'ipw', 'sipw', or, 'bart', 'xbart', 'bcf', 'xbcf', or 'cf'")
   }
 
-  # Check for propensity score estimation
-  if (!(toupper(include_ps_dis) %in% c("TRUE", "FALSE", NA)) |
-      !(toupper(include_ps_inf) %in% c("TRUE", "FALSE", NA))) {
-    stop("Please specify 'TRUE', 'FALSE', or NA for the include_ps_dis and include_ps_inf arguments.")
+  # Check for correct propensity score estimation inputs
+  include_ps_dis <- toupper(include_ps_dis)
+  if (ite_method_dis %in% c("bart", "xbart", "cf")) {
+    if (!(include_ps_dis %in% c(TRUE, FALSE))) {
+      stop("Please specify 'TRUE' or 'FALSE' for the include_ps_dis argument.")
+    }
+  } else {
+    include_ps_dis <- NA
+  }
+  include_ps_inf <- toupper(include_ps_inf)
+  if (ite_method_inf %in% c("bart", "xbart", "cf")) {
+    if (!(include_ps_inf %in% c(TRUE, FALSE))) {
+      stop("Please specify 'TRUE' or 'FALSE' for the include_ps_inf argument.")
+    }
+  } else {
+    include_ps_inf <- NA
   }
 
-  # Check for binary outcome
+  # Determine outcome type
   binary <- ifelse(length(unique(y)) == 2, TRUE, FALSE)
-  if (tolower(ite_method_dis) == "bcf" | tolower(ite_method_inf) == "bcf") {
-    stop("The Bayesian Causal Forest method does not apply to binary outcomes.
-         Please select another method to estimate the ITE.")
+  if (binary) {
+    if (ite_method_dis %in% c("bcf", "xbcf", "ipw", "sipw") |
+        ite_method_inf %in% c("bcf", "xbcf", "ipw", "sipw")) {
+      stop("The 'ipw', 'sipw', 'bcf', and 'xbcf' methods are not applicable to data with binary outcomes.
+           Please select a method from the following: 'or', 'cf', 'bart', or 'xbart'")
+    }
+  }
+
+  # Check for correct rules_method input
+  rules_method <- tolower(rules_method)
+  if (binary) {
+    if (!(rules_method %in% c("conservative", "anticonservative"))) {
+      stop("Invalid rules_method input. Please specify 'conservative' or 'anticonservative'.")
+    }
+  } else {
+    rules_method <- NA
   }
 
   # Step 1: Split data
@@ -88,7 +119,7 @@ cre <- function(y, z, X, ratio_dis, ite_method_dis, ite_method_inf,
 
   # Step 2: Estimate ITE
   message("Step 2: Estimating ITE")
-  ite_list_dis <- estimate_ite(y_dis, z_dis, X_dis, ite_method_dis, include_ps_dis)
+  ite_list_dis <- estimate_ite(y_dis, z_dis, X_dis, ite_method_dis, include_ps_dis, binary)
   ite_dis <- ite_list_dis[["ite"]]
   ite_std_dis <- ite_list_dis[["ite_std"]]
 
@@ -105,7 +136,8 @@ cre <- function(y, z, X, ratio_dis, ite_method_dis, ite_method_inf,
 
   # Step 5: Select important rules
   message("Step 5: Selecting Important Causal Rules")
-  select_rules_dis <- select_causal_rules(rules_matrix_std_dis, rules_list_dis, ite_std_dis, binary)
+  select_rules_dis <- select_causal_rules(rules_matrix_std_dis, rules_list_dis,
+                                          ite_std_dis, binary, q, rules_method)
   select_rules_matrix_dis <- rules_matrix_dis[,which(rules_list_dis %in% select_rules_dis)]
   select_rules_matrix_std_dis <- rules_matrix_std_dis[,which(rules_list_dis %in% select_rules_dis)]
 
@@ -114,7 +146,7 @@ cre <- function(y, z, X, ratio_dis, ite_method_dis, ite_method_inf,
 
   # Step 2: Estimate ITE
   message("Step 2: Estimating ITE")
-  ite_list_inf <- estimate_ite(y_inf, z_inf, X_inf, ite_method_inf, include_ps_inf)
+  ite_list_inf <- estimate_ite(y_inf, z_inf, X_inf, ite_method_inf, include_ps_inf, binary)
   ite_inf <- ite_list_inf[["ite"]]
   ite_std_inf <- ite_list_inf[["ite_std"]]
 
