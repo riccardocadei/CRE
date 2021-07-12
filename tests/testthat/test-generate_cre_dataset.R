6995964e1d0284f760c2ee22df30fa9c8ceb0b39
Daniela Garcia <danielagarcia@surgofoundation.org>
2021-07-12 12:45:37 -0400

    Fix long lines and remove suppress warnings

--- tests/testthat/test-generate_cre_dataset.R
+++ tests/testthat/test-generate_cre_dataset.R
@@ -1,23 +1,36 @@
 test_that("Synthetic Data Generated Correctly", {
   # Incorrect n input
-  suppressWarnings(expect_error(generate_cre_dataset(n = "test", rho = 0, n_rules = 2, effect_size = 0.5, binary = FALSE, seed = 2021)))
+  expect_error(generate_cre_dataset(n = "test", rho = 0,
+                                    n_rules = 2, effect_size = 0.5,
+                                    binary = FALSE, seed = 2021))
 
   # Incorrect rho input
-  suppressWarnings(expect_error(generate_cre_dataset(n = 1000, rho = "test", n_rules = 2, effect_size = 0.5, binary = FALSE, seed = 2021)))
+  expect_error(generate_cre_dataset(n = 1000, rho = "test",
+                                    n_rules = 2, effect_size = 0.5,
+                                    binary = FALSE, seed = 2021))
 
   # Incorrect number of rules
-  suppressWarnings(expect_error(generate_cre_dataset(n = 1000, rho = 0, n_rules = 3, effect_size = 0.5, binary = FALSE, seed = 2021)))
-  suppressWarnings(expect_error(generate_cre_dataset(n = 1000, rho = 0, n_rules = "test", effect_size = 0.5, binary = FALSE, seed = 2021)))
+  expect_error(generate_cre_dataset(n = 1000, rho = 0,
+                                    n_rules = 3, effect_size = 0.5,
+                                    binary = FALSE, seed = 2021))
+  expect_error(generate_cre_dataset(n = 1000, rho = 0,
+                                    n_rules = "test", effect_size = 0.5,
+                                    binary = FALSE, seed = 2021))
 
   # Incorrect effect_size input
-  suppressWarnings(expect_error(generate_cre_dataset(n = 1000, rho = 0, n_rules = 2, effect_size = "test", binary = FALSE, seed = 2021)))
+  expect_error(generate_cre_dataset(n = 1000, rho = 0,
+                                    n_rules = 2, effect_size = "test",
+                                    binary = FALSE, seed = 2021))
 
   # Incorrect binary input
-  suppressWarnings(expect_error(generate_cre_dataset(n = 1000, rho = 0, n_rules = 2, effect_size = 0.5, binary = "test", seed = 2021)))
+  expect_error(generate_cre_dataset(n = 1000, rho = 0,
+                                    n_rules = 2, effect_size = 0.5,
+                                    binary = "test", seed = 2021))
 
   # Correct outputs
   n <- 1000
-  test_data_cont <- generate_cre_dataset(n = n, rho = 0, n_rules = 2, effect_size = 0.5, binary = TRUE, seed = 2021)
+  test_data_cont <- generate_cre_dataset(n = n, rho = 0, n_rules = 2, effect_size = 0.5,
+                                         binary = TRUE, seed = 2021)
   expect_true(class(test_data_cont) == "list")
   expect_true(length(test_data_cont) == 3)
   expect_true(class(test_data_cont[[1]]) == "numeric")
