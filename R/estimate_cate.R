cdbc5c45f64eccbdab6d408bc14897c51793fe9f
Daniela Garcia <danielagarcia@surgofoundation.org>
2021-07-15 14:52:49 -0400

    Remove means estimation for CATE

--- R/estimate_cate.R
+++ R/estimate_cate.R
@@ -8,7 +8,7 @@
 #' @param rules_matrix_inf the standardized causal rules matrix for the inference subsample
 #' @param rules_list_inf the list of causal rules for the inference subsample
 #'
-#' @return two sets of CATE estimates, one done by averaging the ITEs and the other using linear regression
+#' @return a matrix of CATE estimates
 #'
 #' @export
 #'
@@ -24,35 +24,33 @@ estimate_cate <- function(ite_inf, rules_matrix_inf, rules_list_inf) {
   names(rules_df_factor) <- rules_list_inf
   joined_ite_rules <- cbind(ite_inf, rules_df_factor)
 
-  # Generate CATE data frame with ATE
-  cate_means <- data.frame(Rule = "Average Treatment Effect", CATE = mean(ite_inf),
-                           CI_lower = mean(ite_inf) - (1.96 * sqrt(stats::var(ite_inf)) / sqrt(length(ite_inf))),
-                           CI_upper = mean(ite_inf) + (1.96 * sqrt(stats::var(ite_inf)) / sqrt(length(ite_inf))))
-
-  # Determine CATE manually for each rule
-  for (i in 1:length(rules_list_inf)) {
-    df_temp <- data.frame(ite = joined_ite_rules[,1], rule = joined_ite_rules[,i + 1]) %>% dplyr::filter(rule == 1)
-    cate_temp <- data.frame(Rule = rules_list_inf[i], CATE = mean(df_temp$ite),
-                            CI_lower = mean(df_temp$ite) - (1.96 * sqrt(stats::var(df_temp$ite)) / sqrt(length(df_temp$ite))),
-                            CI_upper = mean(df_temp$ite) + (1.96 * sqrt(stats::var(df_temp$ite)) / sqrt(length(df_temp$ite))))
-    cate_means <- rbind(cate_means, cate_temp)
-  }
-
-  # Fit linear regression model without contrasts, then extract coefficients and confidence intervals
+  # Fit linear regression model with contr.treatment, then extract coefficients and confidence intervals
+  options(contrasts = rep("contr.treatment", 2))
   model1_cate <- stats::lm(ite_inf ~ ., data = joined_ite_rules)
   model1_coef <- summary(model1_cate)$coef[,c(1,4)] %>% as.data.frame
   model1_ci <- stats::confint(model1_cate) %>% as.data.frame() %>% dplyr::filter(!is.na(.))
 
+  group_means <- vector(length = nrow(model1_coef))
+  group_means[1] <- model1_coef[1,1]
+  for (i in 2:nrow(model1_coef)) {
+    group_means[i] <- model1_coef[1,1] + model1_coef[i,1]
+  }
+
   # Generate model 1 data frame
   cate_reg_orig <- model1_coef %>% cbind(model1_ci)
-  cate_reg_orig_names <- stringr::str_extract(row.names(cate_reg_orig), "`.*`") %>% stringr::str_remove_all("`")
+  cate_reg_orig_names <- stringr::str_extract(row.names(cate_reg_orig), "`.*`") %>%
+    stringr::str_remove_all("`")
   cate_reg_orig_names[1] <- "(Intercept)"
   cate_reg_orig$Rule <- cate_reg_orig_names
   row.names(cate_reg_orig) <- 1:nrow(cate_reg_orig)
-  cate_reg_orig <- cate_reg_orig %>% dplyr::summarize(Rule, CATE = Estimate, PVal = `Pr(>|t|)`,
-                                                      CI_lower = `2.5 %`, CI_upper = `97.5 %`)
+  cate_reg_orig <- cate_reg_orig %>%
+    dplyr::summarize(Rule, Model_Coef = Estimate, CATE = Estimate, PVal = `Pr(>|t|)`,
+                     CI_lower = `2.5 %`, CI_upper = `97.5 %`)
+  for (i in 2:nrow(cate_reg_orig)) {
+    cate_reg_orig[i,3] <- cate_reg_orig[1,2] + cate_reg_orig[i,2]
+  }
+  stopifnot(identical(rules_list_inf, cate_reg_orig$Rule[2:nrow(cate_reg_orig)]))
 
   # Return final results
-  cates <- list(cate_means = cate_means, cate_reg_orig = cate_reg_orig)
-  return(cates)
+  return(cate_reg_orig)
 }
