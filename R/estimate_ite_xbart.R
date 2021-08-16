04309aeaa9eca8cb523a9cab5431d4f1fe8285e4
Daniela Garcia <danielagarcia@surgofoundation.org>
2021-08-16 14:20:35 -0400

    Calculate and return standard deviations

--- R/estimate_ite_xbart.R
+++ R/estimate_ite_xbart.R
@@ -9,7 +9,7 @@
 #' @param X the features matrix
 #' @param include_ps whether or not to include propensity score estimate as a covariate in ITE estimation
 #'
-#' @return a vector of ITE estimates
+#' @return a list of ITE estimates and standard deviations for the ITE estimates
 #'
 #' @export
 #'
@@ -22,18 +22,24 @@ estimate_ite_xbart <- function(y, z, X, include_ps) {
   X_treated <- X[z==1,]
   y_control <- y[z==0]
   X_control <- X[z==0,]
-  xbart_y1 <- XBART::XBART(y = as.matrix(y_treated), X = as.matrix(X_treated), Xtest = as.matrix(X),
-                           num_trees = 30, num_sweeps = 40, max_depth = 250, Nmin = 1,
-                           num_cutpoints = 50, alpha = 0.95, beta = 1.25, tau = stats::var(y_treated) / 30,
-                           no_split_penality = "Auto", burnin = 15, mtry = ncol(X),
-                           p_categorical = ncol(X), kap = 1, s = 1, verbose = FALSE, parallel = TRUE)
-  y1hat <- apply(xbart_y1$yhats_test[, 15:40], 1, mean)
-  xbart_y0 <- XBART::XBART(y = as.matrix(y_control), X = as.matrix(X_control), Xtest = as.matrix(X),
-                           num_trees = 30, num_sweeps = 40, max_depth = 250, Nmin = 1,
-                           num_cutpoints = 50, alpha = 0.95, beta = 1.25, tau = stats::var(y_control) / 30,
-                           no_split_penality = "Auto",  burnin = 15, mtry = ncol(X),
-                           p_categorical = ncol(X), kap = 1, s = 1, verbose = FALSE, parallel = TRUE)
-  y0hat <- apply(xbart_y0$yhats_test[, 15:40], 1, mean)
-  ite <- y1hat - y0hat
-  return(ite)
+
+  xbart_y1 <- XBART::XBART(y = as.matrix(y_treated), X = as.matrix(X_treated),
+                           Xtest = as.matrix(X),num_trees = 30, num_sweeps = 1000,
+                           max_depth = 250, Nmin = 1, num_cutpoints = 50,
+                           alpha = 0.95, beta = 1.25, tau = stats::var(y_treated) / 30,
+                           no_split_penality = "Auto", burnin = 100, mtry = ncol(X),
+                           p_categorical = ncol(X), kap = 1, s = 1, verbose = FALSE,
+                           parallel = TRUE)
+  xbart_y0 <- XBART::XBART(y = as.matrix(y_control), X = as.matrix(X_control),
+                           Xtest = as.matrix(X), num_trees = 30, num_sweeps = 1000,
+                           max_depth = 250, Nmin = 1, num_cutpoints = 50,
+                           alpha = 0.95, beta = 1.25, tau = stats::var(y_control) / 30,
+                           no_split_penality = "Auto", burnin = 100, mtry = ncol(X),
+                           p_categorical = ncol(X), kap = 1, s = 1, verbose = FALSE,
+                           parallel = TRUE)
+
+  pd_ite <- xbart_y1$yhats_test[, 100:1000] - xbart_y0$yhats_test[, 100:1000]
+  ite <- apply(pd_ite, 1, mean)
+  sd_ite <- apply(pd_ite, 1, sd)
+  return(list(ite, sd_ite))
 }
--- R/estimate_ite_xbcf.R
+++ R/estimate_ite_xbcf.R
@@ -8,24 +8,25 @@
 #' @param z the treatment vector
 #' @param X the features matrix
 #'
-#' @return a vector of ITE estimates
+#' @return a list of ITE estimates and standard deviations for the ITE estimates
 #'
 #' @export
 #'
 estimate_ite_xbcf <- function(y, z, X) {
   est_ps <- estimate_ps(z, X)
   xbcf_model <- XBCF::XBCF(y = as.matrix(y), z = as.matrix(z), x_con = as.matrix(X),
-                           x_mod = as.matrix(X), pihat = as.matrix(est_ps), num_sweeps = 40,
-                           burnin = 15, max_depth = 250, num_cutpoints = 20,
+                           x_mod = as.matrix(X), pihat = as.matrix(est_ps),
                            mtry_con = ncol(X), mtry_mod = ncol(X),
                            pcat_con = ncol(X), pcat_mod = ncol(X),
-                           n_trees_con = 30, tau_con = 10 * stats::var(y) / 30,
-                           n_trees_mod = 10, tau_mod = 1 * stats::var(y) / 10)
-  th_xbcf <- xbcf_model$tauhats * stats::sd(y)
+                           n_trees_con = 200, tau_con = 10 * stats::var(y) / 200,
+                           n_trees_mod = 50, tau_mod = 1 * stats::var(y) / 50)
+  pd_ite_temp <- xbcf_model$tauhats * stats::sd(y)
   b_xbcf <- xbcf_model$b_draws
   for (i in 16:40) {
-    th_xbcf[, i] <- th_xbcf[, i] * (b_xbcf[i, 2] - b_xbcf[i, 1])
+    pd_ite_temp[, i] <- pd_ite_temp[, i] * (b_xbcf[i, 2] - b_xbcf[i, 1])
   }
-  ite <- rowMeans(th_xbcf[, 16:40])
-  return(ite)
+  pd_ite <- pd_ite_temp[, 16:40]
+  ite <- apply(pd_ite, 1, mean)
+  sd_ite <- apply(pd_ite, 1, sd)
+  return(list(ite, sd_ite))
 }
