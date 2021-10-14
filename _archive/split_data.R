9c0131778e848264d10e3628b44b2589abceccf1
naeemkh <khoshnevis.naeem@gmail.com>
2021-10-14 14:40:01 -0400

    having progress ...

---
+++ _archive/split_data.R
@@ -0,0 +1,22 @@
+#' @title
+#' Split Data
+#'
+#' @description
+#' Split Data into Discovery and Inference Subsamples
+#'
+#' @param y the observed response vector
+#' @param z the treatment vector
+#' @param X the features matrix
+#' @param ratio_dis the ratio of data delegated to the discovery subsample
+#'
+#' @return a list containing the discovery and inference subsamples
+#'
+#' @export
+#'
+split_data <- function(y, z, X, ratio_dis) {
+  total_sample <- cbind(y, z, X)
+  index <- sample(nrow(total_sample), nrow(total_sample) * ratio_dis)
+  discovery <- total_sample[index,]
+  inference <- total_sample[-index,]
+  return(list(discovery = discovery, inference = inference))
+}
