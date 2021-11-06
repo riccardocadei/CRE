919c3e679f4567c86a3c97c4e88db1e8c04ad21d
Falco J. Bargagli-Stoffi <36726286+fbargaglistoffi@users.noreply.github.com>
2021-11-05 23:16:35 -0400

    More general code to generate synthetic dataset

--- R/generate_cre_dataset.R
+++ R/generate_cre_dataset.R
@@ -19,76 +19,63 @@
 #'
 #' @examples
 #' set.seed(123)
-#' dataset_c <- generate_cre_dataset(n = 500, rho = 0, n_rules = 2,
-#' effect_size = 0.5, binary = FALSE)
+#' dataset_c <- generate_cre_dataset(n = 1000, rho = 0, n_rules = 2, p = 10,
+#' effect_size = 2, binary = FALSE)
 #'
 #' @export
 #'
-generate_cre_dataset <- function(n, rho = 0, n_rules = 2,
-                                 effect_size = 0.5, binary = TRUE) {
 
+generate_cre_dataset <- function(n = 1000, rho = 0, n_rules = 2, p = 10,
+                                 effect_size = 2, binary = FALSE) {
+  
   # Check for correct binary input
   if (!(binary %in% c(TRUE, FALSE))) {
     stop("Invalid 'binary' input. Please specify TRUE or FALSE.")
   }
-
+  
   # Generate Variables
-  p <- 10
   mu <- rep(0, p)
   Sigma <- matrix(rho, nrow = p, ncol = p) + diag(p) * (1 - rho)
   rawvars <- MASS::mvrnorm(n = n, mu = mu, Sigma = Sigma)
   pvars <- stats::pnorm(rawvars)
   X <- stats::qbinom(pvars, 1, 0.5)
-  colnames(X) <- paste("X", 1:10, sep = "")
-  x1 <- X[,1]
-  x2 <- X[,2]
-  x3 <- X[,3]
-  x4 <- X[,4]
-  x5 <- X[,5]
-  x6 <- X[,6]
-  x7 <- X[,7]
-  x8 <- X[,8]
-  x9 <- X[,9]
-  x10 <- X[,10]
-
+  colnames(X) <- paste("x", 1:p, sep = "")
+  X <- as.data.frame(X)
+  
   # Generate Causal Rules and Treatment Effects
-  if (binary) {
+  if (binary == TRUE) {
     y0 <- rep(0, n)
-    y0[x2 == 1 & x3 == 1] <- 1
+    y0[X$x2 == 1 & X$x3 == 1] <- 1
     y1 <- rep(0, n)
-    y1[x1 == 0 & x2 == 0] <- 1
+    y1[X$x1 == 0 & X$x2 == 0] <- 1
     tau <- y1 - y0
   } else {
     stopifnot(n_rules %in% c(2, 4))
     if (n_rules == 2) {
       tau <- rep(0, n)
-      tau[x1 == 0 & x2 == 0] = effect_size
-      tau[x2 == 1 & x3 == 1] = 3 * effect_size
+      tau[X$x1 == 0 & X$x2 == 0] = effect_size
+      tau[X$x2 == 1 & X$x3 == 1] = - effect_size
     } else {
       tau <- rep(0, n)
-      tau[x1 == 0 & x2 == 0] = effect_size
-      tau[x1 == 1 & x2 == 1] = 3 * effect_size
-      tau[x2 == 0 & x3 == 0] = effect_size
-      tau[x2 == 1 & x3 == 1] = 3 * effect_size
+      tau[X$x1 == 0 & X$x2 == 0] = effect_size
+      tau[X$x1 == 1 & X$x2 == 1] = - effect_size
+      tau[X$x2 == 0 & X$x3 == 0] = effect_size
+      tau[X$x2 == 1 & X$x3 == 1] = - effect_size
     }
-    y0 <- stats::rnorm(n, mean = x1 + 0.5 * x2 + x3, sd = 1)
+    y0 <- stats::rnorm(n, mean = X$x1 + 0.5 * X$x2 + X$x3, sd = 1)
     y1 <- y0 + tau
   }
-
+  
   # Generate Treatment Variable
-  logit.prob <- -1 + x1 - x2 + x3
+  logit.prob <- -1 + X$x1 - X$x2 + X$x3
   prob <- exp(logit.prob) / (1 + exp(logit.prob))
-
+  
   # Generate Treatment Indicator
   z <- stats::rbinom(n, 1, prob = prob)
-
+  
   # Generate Outcome
-  if (binary) {
-    y <- y0 * (1 - z) + y1 * z
-  } else {
-    y <- y0 * (1 - z) + y1 * z + x1 - x2 + x3
-  }
-
+  y <- y0 * (1 - z) + y1 * z
+  
   # Observed Data
   dataset <- list(y = y, z = z, X = X)
   names(dataset) <- c("y", "z", "X")
