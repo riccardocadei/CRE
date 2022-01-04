7cdec2f462ec22d831389b8cb2864e624ad64624
Falco J. Bargagli-Stoffi <36726286+fbargaglistoffi@users.noreply.github.com>
2022-01-04 15:31:35 +0100

    Update estimate_ps: add estimation method in the function's statement

--- R/estimate_ps.R
+++ R/estimate_ps.R
@@ -14,19 +14,20 @@
 #' @import SuperLearner
 #'
 #' @examples
-#' dataset_cont <- generate_cre_dataset(n = 1000, rho = 0, n_rules = 2,
+#' dataset_cont <- generate_cre_dataset(n = 1000, rho = 0, n_rules = 2, p = 10,
 #'                                      effect_size = 2, binary = FALSE)
 #'
 #' # Initialize parameters
 #' z <- dataset_cont[["z"]]
 #' X <- as.data.frame(dataset_cont[["X"]])
+#' method_ps <- "SL.xgboost"
 #'
-#' est_ps <- estimate_ps(z, X)
+#' est_ps <- estimate_ps(z, X, method_ps)
 #'
-estimate_ps <- function(z, X) {
+estimate_ps <- function(z, X, method_ps = "SL.xgboost") {
   sl_pscore <- SuperLearner(Y = z, X = as.data.frame(X),
                                           newX = as.data.frame(X), family = binomial(),
-                                          SL.library = "SL.xgboost", cvControl = list(V=0))
+                                          SL.library = method_ps, cvControl = list(V=0))
   est_ps <- as.numeric(sl_pscore$SL.predict)
   return(est_ps)
 }
