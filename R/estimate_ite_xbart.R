23e317b527167d18f54bc687e1f87f2cf67a8daa
naeemkh <khoshnevis.naeem@gmail.com>
2022-09-19 18:09:29 -0400

    deactivate xbart's example.

--- R/estimate_ite_xbart.R
+++ R/estimate_ite_xbart.R
@@ -14,21 +14,6 @@
 #'
 #' @export
 #'
-#' @examples
-#' dataset <- generate_cre_dataset(n = 1000, rho = 0, n_rules = 2, p = 10,
-#'                                 effect_size = 2, binary = FALSE)
-#'
-#' # Initialize parameters
-#' y <- dataset[["y"]]
-#' z <- dataset[["z"]]
-#' X <- as.data.frame(dataset[["X"]])
-#' include_ps <- TRUE
-#' ps_method <- "SL.xgboost"
-#'
-#' if (requireNamespace("XBART", quietly = TRUE)) {
-#'     ite_list <- estimate_ite_xbart(y, z, X, include_ps, ps_method)
-#' }
-#'
 estimate_ite_xbart <- function(y, z, X, include_ps, ps_method) {
 
   if (!requireNamespace("XBART", quietly = TRUE)) {
