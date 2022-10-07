2708a9e0f6f20d56faed75d03f9bafe19b23e542
Riccardo Cadei <47679062+riccardocadei@users.noreply.github.com>
2022-10-07 04:23:51 -0400

    fix check fail 2

--- tests/testthat/test-generate_causal_rules.R
+++ tests/testthat/test-generate_causal_rules.R
@@ -34,6 +34,7 @@ test_that("generate_causal_rules works as expected!", {
                       type_decay = 2,
                       t_anom = 0.025,
                       t_corr = 1,
+                      replace = FALSE,
                       q = 0.8,
                       stability_selection = TRUE,
                       pfer_val = 0.1)
