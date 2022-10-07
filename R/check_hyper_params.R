bbbd04c043f7e25f768daf6901f90ba6d68f47cd
Riccardo Cadei <47679062+riccardocadei@users.noreply.github.com>
2022-10-07 03:50:59 -0400

    add check for `replace`

--- R/check_hyper_params.R
+++ R/check_hyper_params.R
@@ -30,6 +30,10 @@ check_hyper_params <- function(params){
     stop("Invalid 'max_nodes' input. Please input a number.")
   }
 
+  if (!(getElement(params, "replace") %in% c(TRUE, FALSE))) {
+    stop("Please specify 'TRUE' or 'FALSE' for the replace argument.")
+  }
+
   if (!inherits(getElement(params, "t_anom"),"numeric")){
     stop("Invalid 't' input. Please input a number.")
   }
