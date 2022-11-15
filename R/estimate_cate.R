015c89c4bb73906586c4a94b341da0ec9573b9b1
Riccardo Cadei <47679062+riccardocadei@users.noreply.github.com>
2022-11-15 01:56:36 -0500

    add debug print for other OS error

--- R/estimate_cate.R
+++ R/estimate_cate.R
@@ -171,6 +171,8 @@ estimate_cate <- function(y_inf, z_inf, X_inf, X_names, include_offset,
         cbind(cate_model)
       cate_final <- subset(cate_temp, cate_temp$P_Value <= t_pvalue |
                                       cate_temp$Rule == "(BATE)")
+      print('debug')
+      print(cate_final)
       rownames(cate_final) <- 1:nrow(cate_final)
     } else if (cate_method == "bart-baggr") {
 
