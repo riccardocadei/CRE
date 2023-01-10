8c8d2c328239cbcb2b4465204cfd52175f780115
Riccardo Cadei <47679062+riccardocadei@users.noreply.github.com>
2023-01-09 22:55:03 -0500

    improve `test-estimate_cate`

--- tests/testthat/test-estimate_cate.R
+++ tests/testthat/test-estimate_cate.R
@@ -1,4 +1,4 @@
-test_that("CATE (DRLearner) Estimation Runs Correctly", {
+test_that("CATE Estimation Runs Correctly (test 1/2)", {
   # Generate sample data
   set.seed(2021)
   dataset_cont <- generate_cre_dataset(n = 500, rho = 0, n_rules = 2, p = 10,
@@ -25,7 +25,7 @@ test_that("CATE (DRLearner) Estimation Runs Correctly", {
   max_decay <- 0.025
   type_decay <- 2
   t_ext <- 0.02
-  t_corr <- 1
+  t_corr <- 0
   t_pvalue <- 0.05
   cutoff <- 0.8
   pfer <- 0.1
@@ -55,7 +55,7 @@ test_that("CATE (DRLearner) Estimation Runs Correctly", {
   z_inf <- inference$z
   X_inf <- inference$X
 
-  # Step 2: Estimate ITE
+  # Estimate ITE
   ite_list_dis <- estimate_ite(y_dis, z_dis, X_dis, ite_method_dis,
                                binary_outcome,
                                include_ps = include_ps_dis,
@@ -66,7 +66,7 @@ test_that("CATE (DRLearner) Estimation Runs Correctly", {
   ite_dis <- ite_list_dis[["ite"]]
 
 
-  # Step 3: Generate rules list
+  # Generate rules list
   initial_rules_dis <- generate_rules(X_dis, ite_dis, intervention_vars,
                                       ntrees_rf, ntrees_gbm, node_size,
                                       max_nodes, max_depth, replace)
@@ -74,24 +74,24 @@ test_that("CATE (DRLearner) Estimation Runs Correctly", {
   rules_list_dis <- filter_irrelevant_rules(initial_rules_dis, X_dis,
                                             ite_dis, max_decay, type_decay)
 
-  # Step 4: Generate rules matrix
+  # Generate rules matrix
   rules_matrix_dis <- generate_rules_matrix(X_dis, rules_list_dis)
-  rules_matrix_std_dis <- standardize_rules_matrix(rules_matrix_dis)
 
-  # Step 5: Select important rules
-  select_rules_dis <- as.character(select_rules(rules_matrix_std_dis,
+  # Filter extreme rules
+  rules_matrix_dis <- filter_extreme_rules(rules_matrix_dis, rules_list_dis,
+                                           t_ext)
+  rules_list_dis <- colnames(rules_matrix_dis)
+
+  # Select important rules
+  select_rules_dis <- as.character(select_rules(rules_matrix_dis,
                                                 rules_list_dis,
                                                 ite_dis,
                                                 stability_selection,
                                                 cutoff,
                                                 pfer,
                                                 penalty_rl))
-  select_rules_matrix_dis <- rules_matrix_dis[,which(rules_list_dis %in%
-                                                       select_rules_dis)]
-  select_rules_matrix_std_dis <- rules_matrix_std_dis[,which(rules_list_dis %in%
-                                                       select_rules_dis)]
 
-  # Step 6: Estimate CATE
+  # Estimate CATE
   ite_list_inf <- estimate_ite(y_inf, z_inf, X_inf, ite_method_inf,
                                binary_outcome,
                                include_ps = include_ps_inf,
@@ -100,8 +100,6 @@ test_that("CATE (DRLearner) Estimation Runs Correctly", {
                                X_names = X_names,
                                offset = offset)
   ite_inf <- ite_list_inf[["ite"]]
-  ite_std_inf <- ite_list_inf[["ite_std"]]
-  sd_ite_inf <- ite_list_inf[["sd_ite"]]
 
   if (length(select_rules_dis)==0){
     rules_matrix_inf <- NA
@@ -121,3 +119,95 @@ test_that("CATE (DRLearner) Estimation Runs Correctly", {
                             ite_inf, t_pvalue)
   expect_true(class(cate_inf$summary) == "data.frame")
 })
+
+
+test_that("CATE Estimation Runs Correctly (test 2/2)", {
+  # Generate sample data
+  set.seed(2021)
+  dataset_cont <- generate_cre_dataset(n = 500, rho = 0, n_rules = 2, p = 10,
+                                       effect_size = 2, binary_outcome = FALSE)
+  y <- dataset_cont[["y"]]
+  z <- dataset_cont[["z"]]
+  X <- as.data.frame(dataset_cont[["X"]])
+  X_names <- names(as.data.frame(X))
+  ratio_dis <- 0.25
+  ite_method_dis <- "aipw"
+  include_ps_dis <- "TRUE"
+  ps_method_dis <- "SL.xgboost"
+  oreg_method_dis <- "SL.xgboost"
+  ite_method_inf <- "aipw"
+  include_ps_inf <- "FALSE"
+  ps_method_inf <- "SL.xgboost"
+  oreg_method_inf <- "SL.xgboost"
+  ntrees_rf <- 100
+  ntrees_gbm <- 50
+  node_size <- 20
+  max_nodes <- 5
+  max_depth <- 3
+  replace <- FALSE
+  max_decay <- 0.025
+  type_decay <- 2
+  t_ext <- 0.02
+  t_corr <- 0
+  t_pvalue <- 0.05
+  cutoff <- 0.8
+  pfer <- 1
+  stability_selection <- TRUE
+  offset <- NULL
+  t_pvalue <- 0.5
+  intervention_vars <- NULL
+  penalty_rl <- 1
+
+  # Check for binary outcome
+  binary_outcome <- ifelse(length(unique(y)) == 2, TRUE, FALSE)
+
+  # Step 1: Split data
+  X <- as.matrix(X)
+  y <- as.matrix(y)
+  z <- as.matrix(z)
+  subgroups <- honest_splitting(y, z, X, ratio_dis)
+  discovery <- subgroups[[1]]
+  inference <- subgroups[[2]]
+
+  # Generate y, z, and X for discovery and inference data
+  y_dis <- discovery$y
+  z_dis <- discovery$z
+  X_dis <- discovery$X
+
+  y_inf <- inference$y
+  z_inf <- inference$z
+  X_inf <- inference$X
+
+
+  # No rule is selected
+  select_rules_dis <- c()
+
+  # Estimate CATE
+  ite_list_inf <- estimate_ite(y_inf, z_inf, X_inf, ite_method_inf,
+                               binary_outcome,
+                               include_ps = include_ps_inf,
+                               ps_method = ps_method_inf,
+                               oreg_method = oreg_method_inf,
+                               X_names = X_names,
+                               offset = offset)
+  ite_inf <- ite_list_inf[["ite"]]
+
+  if (length(select_rules_dis)==0){
+    rules_matrix_inf <- NA
+    select_rules_interpretable <- NA
+  } else {
+    rules_matrix_inf <- generate_rules_matrix(X_inf, select_rules_dis)
+    select_rules_interpretable <- interpret_rules(select_rules_dis, X_names)
+  }
+
+  ###### Run Tests ######
+
+  # TO DO: add test to check wrong arguments
+
+  # Correct outputs
+  cate_inf <- estimate_cate(y_inf, z_inf, X_inf,
+                            rules_matrix_inf, select_rules_interpretable,
+                            ite_inf, t_pvalue)
+  expect_true(class(cate_inf$summary) == "data.frame")
+})
+
