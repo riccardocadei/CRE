e1263e3f773c47ad12e745ed7ee427d488d2518f
Riccardo Cadei <47679062+riccardocadei@users.noreply.github.com>
2023-01-11 00:27:24 -0500

    add X-Learner for ITE estimation

---
+++ tests/testthat/test-estimate_ite_tlearner.R
@@ -0,0 +1,26 @@
+test_that("T-Learner ITE Estimated Correctly", {
+  # Generate sample data
+  set.seed(8697)
+  dataset_cont <- generate_cre_dataset(n = 100, rho = 0, n_rules = 2, p = 10,
+                                       effect_size = 0.5,
+                                       binary_outcome = FALSE)
+  y <- dataset_cont[["y"]]
+  z <- dataset_cont[["z"]]
+  X <- dataset_cont[["X"]]
+  or_method <- "SL.xgboost"
+
+  # Incorrect data inputs
+  expect_error(estimate_ite_tlearner(y = "test", z, X, or_method))
+  expect_error(estimate_ite_tlearner(y, z = "test", X, or_method))
+  expect_error(estimate_ite_tlearner(y, z, X = NA, or_method))
+
+  # Correct outputs
+  ite <- expect_warning(estimate_ite_tlearner(y, z, X, or_method))
+  expect_true(length(ite) == length(y))
+  expect_true(class(ite) == "numeric")
+
+  # Reproducible results
+  expect_equal(ite[1], 0.02196484, tolerance = 0.00001)
+  expect_equal(ite[11], -1.362927, tolerance = 0.00001)
+  expect_equal(ite[91], 0.1413196, tolerance = 0.00001)
+})
