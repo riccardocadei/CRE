d89422d596c6af0b152a4f91009164ed77b4d7d8
Riccardo Cadei <47679062+riccardocadei@users.noreply.github.com>
2023-01-11 11:03:26 -0500

    improve functional test

--- functional_tests/ft_cre.R
+++ functional_tests/ft_cre.R
@@ -8,12 +8,12 @@ dataset <- generate_cre_dataset(n = 2000,
                                 binary_covariates = TRUE,
                                 binary_outcome = FALSE)
 
-y <- abs(dataset[["y"]])
+y <- dataset[["y"]]
 z <- dataset[["z"]]
 X <- dataset[["X"]]
 
 method_params = list(ratio_dis = 0.25,
-                     ite_method_dis = "tpoisson",
+                     ite_method_dis = "xlearner",
                      ps_method_dis = "SL.xgboost",
                      ps_method_inf = "SL.xgboost",
                      oreg_method_dis = "SL.xgboost",
@@ -21,7 +21,7 @@ method_params = list(ratio_dis = 0.25,
                      ite_method_inf = "xlearner")
 
 hyper_params = list(intervention_vars = NULL,
-                    offset = "x1",
+                    offset = NULL,
                     ntrees_rf = 20,
                     ntrees_gbm = 20,
                     node_size = 20,
