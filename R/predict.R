fc7e4565595d0ce952df4253686d9e39bdfbc653
riccardocadei <riccardo.cadei98@gmail.com>
2023-08-15 15:14:44 -0400

    adress PR comment

--- R/predict.R
+++ R/predict.R
@@ -4,22 +4,23 @@
 #' @description
 #' Predict Individual Treatment Effect via Causal Rule Ensemble algorithm.
 #'
-#' @param CRE A cre object from running the CRE function.
+#' @param object A cre object from running the CRE function.
 #' @param X A covariate matrix (or data.frame)
+#' @param ... Additional arguments passed to customize the prediction.
 #'
 #' @return
 #' Array with the estimated Individual Treatment Effects
 #'
 #' @export
 #'
-predict.cre <- function(CRE, X) {
-  if (is.null(CRE$rules)){
-    ite_pred <- rep(CRE$CATE$Estimate[1], times = nrow(X))
+predict.cre <- function(object, X, ...) {
+  if (is.null(object$rules)){
+    ite_pred <- rep(object$CATE$Estimate[1], times = nrow(X))
   } else {
-    rules_matrix <- generate_rules_matrix(X, CRE$rules)
-    rownames(CRE$CATE) <- CRE$CATE$Rule
-    ite_pred <- rules_matrix %*% as.matrix(CRE$CATE[2:nrow(CRE$CATE),]["Estimate"])
-                + CRE$CATE$Estimate[1]
+    rules_matrix <- generate_rules_matrix(X, object$rules)
+    rownames(object$CATE) <- object$CATE$Rule
+    ite_pred <- rules_matrix %*% as.matrix(object$CATE[2:nrow(object$CATE),]["Estimate"])
+                + object$CATE$Estimate[1]
   }
   return(ite_pred)
 }
