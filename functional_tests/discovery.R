209f3c8aacf754fb5a9a54afd163c983ef72ee2e
riccardocadei <riccardo.cadei98@gmail.com>
2023-08-18 15:59:48 -0400

    update data generating process

--- functional_tests/discovery.R
+++ functional_tests/discovery.R
@@ -33,7 +33,7 @@ if (experiment=="main") {
   sample_size <- 2000
   confounding <- "lin"
   dr <- c("x1>0.5 & x2<=0.5", "x5>0.5 & x6<=0.5",
-          "x4>0.5", "x5<=0.5 & x7>0.5 & x8<=0.5")
+          "x4<=0.5", "x5<=0.5 & x7>0.5 & x8<=0.5")
   em <- c("x1","x2","x5","x6","x4","x7","x8")
   max_depth <- 3
 } else if (experiment=="rct") {
