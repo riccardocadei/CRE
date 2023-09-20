64e3d616c071325674b5d51525205c89d66f847e
naeemkh <khoshnevis.naeem@gmail.com>
2023-09-20 09:53:02 -0400

    add log_level detaisl

--- R/logger_utils.R
+++ R/logger_utils.R
@@ -6,19 +6,38 @@
 #'
 #' @param logger_file_path A path (including file name) to log the messages.
 #' (Default: CRE.log)
-#' @param logger_level The log level. Available levels include:
-#'   - TRACE
-#'   - DEBUG
-#'   - INFO (Default)
-#'   - SUCCESS
-#'   - WARN
-#'   - ERROR
-#'   - FATAL
+#' @param logger_level The log level. When a log level is set, all log levels
+#' below it are also activated (if implemented). Available levels include:
+#'   - TRACE: Provides verbose detailed logging, including the steps taken to
+#'   achieve a result, often used for debugging. Activating TRACE will also
+#'   enable DEBUG, INFO, SUCCESS, WARN, ERROR, and FATAL logs.
+#'   - DEBUG: Provides detailed logging about the flow of the application, used
+#'   mostly by developers to understand potential issues. Activating DEBUG will
+#'   also enable INFO, SUCCESS, WARN, ERROR, and FATAL logs.
+#'   - INFO (Default): Standard messages that inform the user about the normal
+#'   operation of the system. Activating INFO will also enable SUCCESS, WARN,
+#'   ERROR, and FATAL logs.
+#'   - SUCCESS: Messages indicating successful completion of a particular
+#'   operation or task. Activating SUCCESS will also enable WARN, ERROR, and
+#'   FATAL logs.
+#'   - WARN: Warning messages about events that might cause problems in the
+#'   future, but are not yet errors. Activating WARN will also enable ERROR
+#'   and FATAL logs.
+#'   - ERROR: Reports an error due to which the system may not be able to
+#'   achieve its functionality, but the application won't halt. Activating
+#'   ERROR will also enable FATAL logs.
+#'   - FATAL: Reports very severe error events that will presumably lead the
+#'   application to abort.
 #'
 #' @export
 #'
 #' @return
 #' No return value. This function is called for side effects.
+#'
+#' @note
+#' Log levels are specified by developers during the initial implementation.
+#' Future developers or contributors can leverage these log levels to better
+#' capture and document the application's processes and events.
 #'
 #' @examples
 #'
@@ -61,6 +80,8 @@ set_logger <- function(logger_file_path = "CRE.log",
 #' @description
 #' Returns current logger settings.
 #'
+#' @seealso
+#' \code{\link{set_logger}} for information on setting the log level and file path.
 #'
 #' @return
 #' Returns a list that includes **logger_file_path** and **logger_level**.
